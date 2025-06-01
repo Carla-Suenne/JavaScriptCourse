48315f2802c2362c3de51c3259057a5a30c43b1c
Carla Suenne <carla.suenne1@gmail.com>
2025-06-01 17:55:16 -0300

    Update challenge1.js

--- modulo1/challenge1.js
+++ modulo1/challenge1.js
@@ -5,4 +5,18 @@ let soma=15+8;
 soma++;
 
 soma *=3;
-console.log(soma);
+console.log(soma); //72
+
+var souninja =true;
+var comida = ["arroz","feijão","ovo"];
+
+console.log(comida[1]);
+
+console.log(soma===myvar)
+
+console.log(myvar <=soma)
+
+function Divisao(x,y){
+    return x/y;
+}
+console.log(Divisao(10,2));
